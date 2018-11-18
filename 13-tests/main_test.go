d3f6222a59c39ff807437e5c901ce7969896e35c
Wilson Júnior <wilsonpjunior@gmail.com>
2018-11-18 20:16:15 -0200

    update testing module

--- 13-tests/main_test.go
+++ 13-tests/main_test.go
@@ -2,30 +2,20 @@ package main
 
 import (
 	"testing"
-
-	"gopkg.in/check.v1"
 )
 
-func Test(t *testing.T) {
-	check.TestingT(t)
+func TestAddSuccess(t *testing.T) {
+	calculator := &Calculator{}
+	result := calculator.Add(42, 10)
+	if result != 52 {
+		t.Error("Result of 42 + 10 must be 52")
+	}
 }
 
-type CalculatorSuite struct {
-	calculator *Calculator
-}
-
-var _ = check.Suite(&CalculatorSuite{})
-
-func (s *CalculatorSuite) SetUpSuite(c *check.C) {
-	s.calculator = &Calculator{}
-}
-
-func (s *CalculatorSuite) TestAddSuccess(c *check.C) {
-	result := s.calculator.Add(42, 10)
-	c.Check(result, check.Equals, 52)
-}
-
-func (s *CalculatorSuite) TestAddFail(c *check.C) {
-	result := s.calculator.Add(42, 10)
-	c.Check(result, check.Equals, 10)
+func TestAddFail(t *testing.T) {
+	calculator := &Calculator{}
+	result := calculator.Add(42, 10)
+	if result != 50 {
+		t.Error("Result of 42 + 10 must be 52")
+	}
 }
